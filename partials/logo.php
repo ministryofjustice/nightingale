66f92ca58e9c888cea538b9eb029923b048e94dd
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-08-18 16:06:43 +0100

    Add Show/Hide Sitename

--- partials/logo.php
+++ partials/logo.php
@@ -16,6 +16,9 @@ $header_layout     = get_theme_mod( 'logo_type', 'transactional' );
 $org_name_checkbox = get_theme_mod( 'org_name_checkbox', 'no' );
 $org_name_field    = get_theme_mod( 'org_name_field' );
 
+$show_sitename = get_theme_mod( 'show_sitename', 'yes' );
+
+
 /* Check if Organisation name should be different from the site name */
 
 $logo_line_1 = 'no' === $org_name_checkbox ? get_bloginfo( 'name' ) : get_theme_mod( 'org_name_field' );
@@ -26,7 +29,7 @@ if ( has_custom_logo() ) {
 	?>
 	<div class="nhsuk-header__logo">
 		<?php the_custom_logo(); ?>
-		<?php if ( get_theme_mod( 'show_sitename' ) === 'yes' ) { ?>
+		<?php if ( $show_sitename === 'yes' ) { ?>
 			<div class="nhsuk-header__transactional-service-name">
 				<a class="nhsuk-header__transactional-service-name--link" href="<?php echo esc_url_raw( get_home_url() ); ?>"><?php echo esc_html( $logo_line_1 ); ?></a>
 			</div>
@@ -40,7 +43,9 @@ else {
 	?>
 	<div class="nhsuk-header__logo">
 		<a class="nhsuk-header__link" href="<?php echo esc_url_raw( get_home_url() ); ?>" aria-label="<?php bloginfo( 'name' ); ?> homepage">
-			<span class="nhsuk-organisation-name"><?php echo esc_html( $logo_line_1 ); ?></span>
+            <?php if ( $show_sitename === 'yes' ) { ?>
+                <span class="nhsuk-organisation-name"><?php echo esc_html( $logo_line_1 ); ?></span>
+            <?php } ?>
 			<span class="nhsuk-organisation-descriptor"><?php echo esc_html( $logo_line_2 ); ?></span>
 		</a>
 	</div>
